7700ae6ed369c8ca8f5f4d9e03abe1a041dea4f3
GameRockUK <luxmore.jamie@googlemail.com>
2020-05-25 15:44:52 +0000

    Fixed password reset template

--- checkout/forms.py
+++ checkout/forms.py
@@ -26,3 +26,4 @@ class OrderForm(forms.ModelForm):
             'country'
         )
 
+
--- checkout/migrations/0001_initial.py
+++ checkout/migrations/0001_initial.py
@@ -19,7 +19,7 @@ class Migration(migrations.Migration):
             name='Order',
             fields=[
                 ('id', models.AutoField(
-                    auto_created=True, primary_key=True, 
+                    auto_created=True, primary_key=True,
                     serialize=False, verbose_name='ID')),
                 ('full_name', models.CharField(max_length=40)),
                 ('phone_number', models.CharField(max_length=20)),
