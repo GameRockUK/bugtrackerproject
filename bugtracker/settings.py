88a1abeb26c582093dff148e5802f8c36a1aa1cd
GameRockUK <luxmore.jamie@googlemail.com>
2020-05-29 14:06:31 +0000

    Updated allowed_hosts in settings.py

--- bugtracker/settings.py
+++ bugtracker/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get, 'webgraphix.herokuapp.com']
+ALLOWED_HOSTS = ['webgraphix.herokuapp.com']
 
 
 # Application definition
